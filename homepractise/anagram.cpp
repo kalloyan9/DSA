5f86150dbfab1da29ccd9c688bcedc52de486985
Kalloyan9 <kalloyan9@gmail.com>
2022-12-05 19:09:02 +0200

    introduce O(N) solution ... with array.

--- homepractise/anagram.cpp
+++ homepractise/anagram.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <map>
 
-bool isAnagram(std::string& lhs, const std::string& rhs)
+bool isAnagram_NLogN(const std::string& lhs, const std::string& rhs)
 {
     bool result = false;
     const char *p = &lhs[0];
@@ -38,12 +38,46 @@ bool isAnagram(std::string& lhs, const std::string& rhs)
     return result;
 }
 
+bool isAnagram(const std::string& lhs, const std::string& rhs)
+{
+    constexpr int maxChar = 128;
+    bool result = false;
+    const char *p = &lhs[0];
+    int array[maxChar] = {0,};
+    while (*p != '\0') {
+        ++array[*p++];
+    }
+
+    p = &rhs[0];
+    while (*p != '\0') {
+        if (array[*p] != 0) {
+            --array[*p];
+            result = true;
+        } else {
+            return false;
+        }
+        ++p;
+    }
+
+    // if we still have words that are not used.
+    for (int i = 0; i < maxChar; ++i) {
+        if (array[i] != 0) {
+            return false;
+        }
+    }
+
+    return result;
+}
+
 int main ()
 {
-    // dev test:
+    // dev tests:
+    /*
     std::string l = "kurra", r = "rukar";
 
-    std::cout << isAnagram(l, r) << std::endl;
+    std::cout << isAnagram_NLogN(l, r) << std::endl;
 
+    std::cout << isAnagram(l, r) << std::endl;
+    */
     return 0;
 }
